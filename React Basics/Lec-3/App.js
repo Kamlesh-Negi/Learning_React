8f9131742454d7525672d723a4e6458b4d591535
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-02-14 10:40:34 +0530

    functional component

--- React Basics/Lec-3/App.js
+++ React Basics/Lec-3/App.js
@@ -13,7 +13,57 @@ console.log("heading is a type of ", typeof heading);
 // JSX
 const heading2 = <h2>Hello from JSX</h2>
 
+// React Functional Component:
+
+// Different ways of returning from a component 
+
+// method 1
+const HeadingComponent = () => {
+    return <h1>A Functional Component</h1> 
+}
+
+// method 2
+const HeadingComponent2 = () => <h2>A funtional component</h2>
+
+//method 3
+const MyComponent = () => (
+  <div id="container"> 
+    <h1 className="heading">Hello World!</h1>
+    <p>This is a Multi-line Expression in MyComponent</p>
+  </div>
+);
+
+
+// Rendering the HeadingComponent inside MyComponent
+
+const HeadingComponentInsideMyComponent = () => (
+  <div id="container">
+    <h1 className="heading">Hello World!</h1>
+    <p>This is a Multi-line Expression in MyComponent</p>
+  <HeadingComponent/>
+  </div>
+);
+
+
+// Putting a JavaScript variable inside JSX
+
+ const number = 1000
+ 
+ const MyComponentTwo = () => (
+    <div id = "container">
+    {heading2}
+      {number}
+    <h1>JavaScript expression</h1>
+    <h2>{750*4}</h2>
+    {<span>React Element here</span>}
+    {MyComponent()}                            {/**We can call our functional component here like this also */}
+    </div>
+  );
 
 console.log("heading is a type of", typeof heading2);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading2);
+//root.render(heading2); 
+//root.render(<HeadingComponent/>); 
+//root.render(<MyComponent/>); 
+//root.render(<HeadingComponentInsideMyComponent/>); 
+root.render(<MyComponentTwo/>); 
