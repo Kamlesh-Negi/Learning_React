5b73b94af8ee421b91dbecb3b7012f4933894e38
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-02-14 22:49:00 +0530

    FoodOrderingApp

---
+++ React Basics/Lec-4/App.js
@@ -0,0 +1,91 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav bar
+ * Body
+ *  - Search
+ *  - Restaurant Container
+ *      -- Restaurant Card
+ *            -- image
+ *            -- Name of the Restaurant, Ratings, Cuisine etc, Delivery time
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul className="li">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// const styleCard = {
+//   backgroundColor: "grey"
+// }
+
+const RestaurantCard = () => {
+  return (
+    <div className="restaurant-card" style={{ backgroundColor: "gainsboro" }}>
+      <img
+        className="restaurant-logo"
+        alt="veg-biryani"
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xqwpuhgnsaf18te7zvtv"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>30 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
