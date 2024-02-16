1a80a1b818cc5656a4aac1ad1f1396ee856ef02c
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-02-16 21:26:37 +0530

    restaurant data

--- React Basics/Lec-4/App.js
+++ React Basics/Lec-4/App.js
@@ -43,21 +43,90 @@ const Header = () => {
 //   backgroundColor: "grey"
 // }
 
-const RestaurantCard = (props) => {
-  console.log(props);
+
+//  const RestaurantCard = (props) => {
+//   const {restaurantName, cuisine, rating, delivery} = props;   // we can also do like this
+
+const RestaurantCard = ({restaurantData}) => {
+
+  //console.log(props);     {/**props are object */}
+  
+  
+  
   return (
     <div className="restaurant-card" style={{ backgroundColor: "gainsboro" }}>
       <img
         className="restaurant-logo"
         alt="veg-biryani"
-        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xqwpuhgnsaf18te7zvtv"
+        src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+restaurantData.info.cloudinaryImageId}
       />
-      <h3>{props.restaurantName}</h3>
-      <h4>{props.cuisine}</h4>
-      <h4>4.4 stars</h4>
-      <h4>30 minutes</h4>
+      <h3>{restaurantData.info.name}</h3>
+      <h4>{restaurantData.info.cuisines.join(", ")}</h4>
+      <h4>{restaurantData.info.costForTwo }</h4>
+      <h4>Ratings: {restaurantData.info.avgRating}</h4>
+      <h4>Delivery: {restaurantData.info.sla.deliveryTime}</h4>
     </div>
   );
+};
+
+
+const resObj = {
+  type: "restaurant",
+  info: {
+    id: "25620",
+    name: "KFC",
+    cloudinaryImageId: "f01666ac73626461d7455d9c24005cd4",
+    locality: "Intermediate Ring Road",
+    areaName: "Ejipura",
+    costForTwo: "₹400 for two",
+    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+    avgRating: 4.2,
+    parentId: "547",
+    avgRatingString: "4.2",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 26,
+      lastMileTravel: 0.8,
+      serviceability: "SERVICEABLE",
+      slaString: "25-30 mins",
+      lastMileTravelString: "0.8 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-16 23:00:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "40% OFF",
+      subHeader: "UPTO ₹80",
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  analytics: {},
+  cta: {
+    link: "https://www.swiggy.com/restaurants/kfc-intermediate-ring-road-ejipura-bangalore-25620",
+    type: "WEBLINK",
+  },
 };
 
 const Body = () => {
@@ -66,20 +135,9 @@ const Body = () => {
       <div className="search">Search</div>
       <div className="restaurant-container">
         <RestaurantCard
-          restaurantName="Meghana Foods"
-          cuisine="Biryani North Indian, Asian"
-          rating="4.5"
+          restaurantData = {resObj}
         />
-        <RestaurantCard
-          restaurantName="KFC"
-          cuisine="Burger, Fast Food "
-          rating="4.3 "
-        />
-        <RestaurantCard
-          restaurantName="Domino's"
-          cuisine="Pizza,Italian "
-          rating="4"
-        />
+       
       </div>
     </div>
   );
