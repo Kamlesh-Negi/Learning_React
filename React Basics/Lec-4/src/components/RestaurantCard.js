67c17867e142f4237bd417a17113bf558fb75422
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-02-18 22:05:26 +0530

    Restaurant Card component

---
+++ React Basics/Lec-4/src/components/RestaurantCard.js
@@ -0,0 +1,29 @@
+import { cdn_URL } from "../utils/constants";
+
+const RestaurantCard = ({ restaurantData }) => {
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    costForTwo,
+    avgRating,
+    sla: { deliveryTime } = ({} = {}), // as deliveryTime is inside sla{} object which itself is inside info{}
+  } = restaurantData?.info;
+
+  return (
+    <div className="restaurant-card" style={{ backgroundColor: "gainsboro" }}>
+      <img
+        className="restaurant-logo"
+        alt="veg-biryani"
+        src={cdn_URL + cloudinaryImageId}
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>Ratings: {avgRating}</h4>
+      <h4>Delivery: {deliveryTime}</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
