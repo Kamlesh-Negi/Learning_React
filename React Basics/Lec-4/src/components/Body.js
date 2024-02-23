c66be1c32834e9ae78b156bb3e4eb0992f81d3ae
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-02-23 22:18:08 +0530

    useSate Hook

--- React Basics/Lec-4/src/components/Body.js
+++ React Basics/Lec-4/src/components/Body.js
@@ -1,13 +1,23 @@
+import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import {useState} from "react";
+
+
 const Body = () => {
+  // Local State Variable
+  const [listOfRestaurants, setListOfRestaurant] = useState(resList)
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("Button Clicked");
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating > 4.2
+            );
+            setListOfRestaurant(filteredList)
           }}
         >
           Top Rated Restaurant
@@ -16,7 +26,7 @@ const Body = () => {
       <div className="restaurant-container">
         {" "}
         {/*using map to iterate over all the objects in the array */}
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             restaurantData={restaurant}
